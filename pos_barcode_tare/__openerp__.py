476064fb8074e8760f5eca31081b0b5141073d46
François Kawala <cielenid@coopiteasy.be>
2020-01-18 12:25:11 +0100

    Minimum viable barcode tare.

--- pos_barcode_tare/__openerp__.py
+++ pos_barcode_tare/__openerp__.py
@@ -18,9 +18,6 @@
     'author': "Le Nid",
     'website': "http://www.lenid.ch",
 
-    # Categories can be used to filter modules in modules listing
-    # Check https://github.com/odoo/odoo/blob/11.0/odoo/addons/base/module/module_data.xml
-    # for the full list
     'category': 'Point of Sale',
     'version': '0.1',
 
@@ -29,12 +26,11 @@
 
     # always loaded
     'data': [
-        # 'security/ir.model.access.csv',
-        'views/views.xml',
         'views/templates.xml',
     ],
-    # only loaded in demonstration mode
-    'demo': [
-        'demo/demo.xml',
+    'qweb': [
+        'static/src/xml/tare_screen.xml',
+        'static/src/xml/open_tare_screen_button.xml',
+
     ],
 }
