a7d76455e3f931ac3a9b6c017ae74318f774ee2e
François Kawala <cielenid@coopiteasy.be>
2020-01-18 10:06:57 +0100

    Cleaning + switch to String.normalize to remove accents.

--- pos_accented_search/__openerp__.py
+++ pos_accented_search/__openerp__.py
@@ -3,20 +3,16 @@
     'name': "pos_accented_search",
 
     'summary': """
-        Make POS product search insensitive to accented characters.""",
+        Makes tjhe POS product search insensitive to accented characters.""",
 
     'description': """
-        make pos product search insensitive to accented characters in the product
-        name. For instance, to search œuf will match oeuf and œuf, café will match
-        cafe and café.  
+        This add-on make pos product search insensitive to accented characters in the product
+        name. For instance, café will match both cafe and café.  
     """,
 
     'author': "Le Nid",
     'website': "http://www.lenid.ch",
-
-    # Categories can be used to filter modules in modules listing
-    # Check https://github.com/odoo/odoo/blob/11.0/odoo/addons/base/module/module_data.xml
-    # for the full list
+    'license': 'AGPL-3',
     'category': 'Point of Sale',
     'version': '0.1',
 
@@ -25,12 +21,6 @@
 
     # always loaded
     'data': [
-        # 'security/ir.model.access.csv',
-        'views/views.xml',
         'views/templates.xml',
-    ],
-    # only loaded in demonstration mode
-    'demo': [
-        'demo/demo.xml',
     ],
 }
--- pos_accented_search/static/src/js/db.js
+++ pos_accented_search/static/src/js/db.js
@@ -4,17 +4,8 @@ odoo.define('pos_accented_search', function (require) {
      var db = require("point_of_sale.DB");
      db.include({
 
-        init: function(options){
-            this._super(options);
-            this.accented_replacement =
-                {"œ": "oe", "á":"a", "á":"a", "ç":"c", "é": "e", "è":"e", "ê": "e", "ë": "e",
-                 "Œ": "OE", "Á":"A", "Á":"A", "Ç":"C", "É": "E", "È":"E", "Ê": "E", "Ë": "E"};
-        },
-
-        remove_accented_characters: function(product, replacement=this.accented_replacement){
-            return product.replace(/[^\w ]/g, function(char) {
-                return replacement[char] || char;
-                });
+        remove_accented_characters: function(product){
+            return product.normalize("NFD").replace(/[\u0300-\u036f]/g, "")
         },
 
         _product_search_string: function(product){
